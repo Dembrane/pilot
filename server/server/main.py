c036201379593ca48b8656f0b14eb76e6f5ff56e
Sameer Pashikanti <sameer.pashikanti@gmail.com>
2024-03-19 21:28:57 +0530

    explicitly mention cors methods

--- server/server/main.py
+++ server/server/main.py
@@ -53,7 +53,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 

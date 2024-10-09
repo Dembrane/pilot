148820d7c9d36dc37dca883f5b73b350d9e1161d
Bram Delisse <80391302+bramdelisse@users.noreply.github.com>
2024-10-09 14:58:18 +0530

    change default to Dutch (#45)

--- docs/next.config.js
+++ docs/next.config.js
@@ -6,7 +6,7 @@ const withNextra = require('nextra')({
 module.exports = withNextra({
   i18n: {
     locales: ["en-US", "nl-NL"],
-    defaultLocale: "en-US",
+    defaultLocale: "nl-NL",
   },
 })
 

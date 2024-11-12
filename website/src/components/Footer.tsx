0d3fb1a930bae0d8ab3dda1d462d3bbed6d35b56
Sameer Pashikanti <sameer.pashikanti@gmail.com>
2024-11-12 14:50:39 +0000

    (website) fit footer

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -5,21 +5,28 @@ import logo from '@/public/logo-circle.png';
 import { getGlobalsByLang } from '@lib/globals';
 import { t } from '@lingui/macro';
 import { initLingui } from '@/initLingui';
+import { Globals } from '@lib/types';
+import { useLingui } from '@lingui/react';
 
 type FooterProps = {
   lang: string;
 };
 
-export const Footer: React.FC<FooterProps> = async ({ lang }) => {
-  const globals = await getGlobalsByLang(lang);
-  initLingui(lang as 'en-US' | 'nl-NL');
+export const FooterContent = ({
+  globals,
+}: {
+  globals: {
+    title: string;
+  };
+}) => {
+  const { i18n } = useLingui();
 
   return (
     <footer className="sticky z-50 mt-48 flex min-h-64 w-screen flex-col items-center justify-between gap-4 self-center border-t border-border bg-transparent pt-4 text-center shadow backdrop-blur-md">
       <div className="flex items-center">
         <Image
           src={logo}
-          alt={t`logo`}
+          alt={i18n._(t`logo`)}
           className="p-2"
           width={80}
           height={80}
@@ -27,9 +34,9 @@ export const Footer: React.FC<FooterProps> = async ({ lang }) => {
         <h1 className="text-4xl">{globals?.title || 'Dembrane'}</h1>
       </div>
       <div className="p-2">
-        <p>{t`Intrigued?`}</p>
+        <p>{i18n._(t`Intrigued?`)}</p>
         <p>
-          {t`Send us an email at:`}{' '}
+          {i18n._(t`Send us an email at:`)}{' '}
           <Link
             className="text-primary hover:bg-secondary"
             href="mailto:info@dembrane.com?subject=sent from dembrane.com"
@@ -37,19 +44,26 @@ export const Footer: React.FC<FooterProps> = async ({ lang }) => {
             info@dembrane.com
           </Link>
         </p>
-        <p>{t`Or give us a call at:`} +310635625130</p>
+        <p>{i18n._(t`Or give us a call at:`)} +310635625130</p>
       </div>
       <div className="mb-4 flex flex-col gap-4 p-2">
         <Link
           className="text-primary hover:bg-secondary"
           href="https://dembrane.notion.site/Privacy-statements-all-languages-fa97a183f9d841f7a1089079e77ffb52?pvs=4"
         >
-          {t`Privacy Statements`}
+          {i18n._(t`Privacy Statements`)}
         </Link>
         <p className="opacity-50">
-          {t`Dembrane B.V. 2023, all rights reserved`}
+          {i18n._(t`Dembrane B.V. 2023, all rights reserved`)}
         </p>
       </div>
     </footer>
   );
+};
+
+export const Footer: React.FC<FooterProps> = async ({ lang }) => {
+  const globals = await getGlobalsByLang(lang);
+  if (!globals) return null;
+
+  return <FooterContent globals={{ title: globals.title ?? '' }} />;
 };

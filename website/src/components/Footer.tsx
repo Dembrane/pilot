b3525e93a763b8cabd42d88333dfd8b1027d1cc3
Sameer <63326129+spashii@users.noreply.github.com>
2024-11-12 20:36:33 +0530

    (website) fit footer (#62)
    * (website) fit footer
    * fix build

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import logo from '@/public/logo-circle.png';
 import { getGlobalsByLang } from '@lib/globals';
-import { t } from '@lingui/macro';
-import { initLingui } from '@/initLingui';
+import { FooterContent } from './FooterContent';
 
 type FooterProps = {
   lang: string;
@@ -12,44 +8,7 @@ type FooterProps = {
 
 export const Footer: React.FC<FooterProps> = async ({ lang }) => {
   const globals = await getGlobalsByLang(lang);
-  initLingui(lang as 'en-US' | 'nl-NL');
+  if (!globals) return null;
 
-  return (
-    <footer className="sticky z-50 mt-48 flex min-h-64 w-screen flex-col items-center justify-between gap-4 self-center border-t border-border bg-transparent pt-4 text-center shadow backdrop-blur-md">
-      <div className="flex items-center">
-        <Image
-          src={logo}
-          alt={t`logo`}
-          className="p-2"
-          width={80}
-          height={80}
-        />
-        <h1 className="text-4xl">{globals?.title || 'Dembrane'}</h1>
-      </div>
-      <div className="p-2">
-        <p>{t`Intrigued?`}</p>
-        <p>
-          {t`Send us an email at:`}{' '}
-          <Link
-            className="text-primary hover:bg-secondary"
-            href="mailto:info@dembrane.com?subject=sent from dembrane.com"
-          >
-            info@dembrane.com
-          </Link>
-        </p>
-        <p>{t`Or give us a call at:`} +310635625130</p>
-      </div>
-      <div className="mb-4 flex flex-col gap-4 p-2">
-        <Link
-          className="text-primary hover:bg-secondary"
-          href="https://dembrane.notion.site/Privacy-statements-all-languages-fa97a183f9d841f7a1089079e77ffb52?pvs=4"
-        >
-          {t`Privacy Statements`}
-        </Link>
-        <p className="opacity-50">
-          {t`Dembrane B.V. 2023, all rights reserved`}
-        </p>
-      </div>
-    </footer>
-  );
+  return <FooterContent globals={{ title: globals.title ?? '' }} />;
 };

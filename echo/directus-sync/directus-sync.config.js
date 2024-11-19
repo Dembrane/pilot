2c0ba5a0ac9c8b179c8595a125012cddb3f3d9be
Sameer Pashikanti <sameer.pashikanti@gmail.com>
2024-11-19 12:09:06 +0100

    chore: seperate website and echo

---
+++ echo/directus-sync/directus-sync.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    directusUrl: 'https://your-directus-instance.com',
+    directusToken: 'your-access-token',
+    dumpPath: './directus-config',
+    // Additional options...
+};

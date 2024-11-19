263227044de2f5b3abe182483c02d3cb031a93ee
Sameer <63326129+spashii@users.noreply.github.com>
2024-11-19 23:04:15 +0530

    (website) add plausible and cookie banner (#71)
    * added plausible tracking
    * script outside head
    
    ---------
    
    Co-authored-by: Jorim <jorim@dembrane.com>

--- website/src/app/[lang]/layout.tsx
+++ website/src/app/[lang]/layout.tsx
@@ -11,6 +11,8 @@ import DembraneBackground from '@/components/animations/DembraneBackground';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { BackgroundProvider } from '@/lib/contexts/BackgroundContext';
 import { Toaster } from '@/components/ui/sonner';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -31,6 +33,8 @@ export default async function RootLayout(props: LayoutProps) {
 
   return (
     <html lang={lang} suppressHydrationWarning>
+
+        <Script defer data-domain="dembrane.com" src="https://plausible.io/js/script.js"></Script>
       <body className={`${spaceGrotesk.className} bg-background`}>
         <ErrorBoundary>
           <LinguiClientProvider

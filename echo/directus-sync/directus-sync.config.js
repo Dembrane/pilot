ad3005d5f2970bd2b6016f1463406d96dc5fca85
Sameer <63326129+spashii@users.noreply.github.com>
2024-11-19 19:41:49 +0530

    restructure folders (#70)
    * chore: seperate website and echo
    * stuff

---
+++ echo/directus-sync/directus-sync.config.js
@@ -0,0 +1,9 @@
+const directusBaseUrl = process.env.DIRECTUS_BASE_URL || 'http://localhost:8055';
+const directusToken = process.env.DIRECTUS_TOKEN || '';
+
+module.exports = {
+    directusUrl: 'http://localhost:8055',
+    directusToken: '',
+    dumpPath: './directus-config',
+    // Additional options...
+};
--- setup-extras.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Installing default-jre for PlantUML"
-sudo apt-get install default-jre -y

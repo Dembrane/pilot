a57fac26934ba31986b3eb70caca17328eecbfb3
Sameer <63326129+spashii@users.noreply.github.com>
2025-01-15 01:28:54 +0530

    add directus sync docs (#123)

--- echo/directus/directus-sync.config.js
+++ echo/directus/directus-sync.config.js
@@ -2,11 +2,11 @@ module.exports = {
   // this is assuming we are in the dev container. which means that
   // this NEEDS to be done after the build is complete
   // OR needs to be run in the same container as the directus server
-  directusUrl: process.env.PUBLIC_URL || "http://localhost:8055",
-  directusEmail: process.env.ADMIN_EMAIL || "admin@dembrane.com",
-  directusPassword: process.env.ADMIN_PASSWORD || "admin",
-  dumpPath: "./sync",
-  preserveIds: ["roles", "policies", "dashboards", "panels"],
+  directusUrl: process.env.PUBLIC_URL || 'http://directus:8055',
+  directusEmail: process.env.ADMIN_EMAIL || 'admin@dembrane.com',
+  directusPassword: process.env.ADMIN_PASSWORD || 'admin',
+  dumpPath: './sync',
+  preserveIds: ['roles', 'policies', 'dashboards', 'panels'],
   hooks: {
     flows: {
       onDump: (flows) => {
@@ -23,7 +23,7 @@ module.exports = {
       },
       onLoad: (flows) => {
         return flows.map((flow) => {
-          flow.name = flow.name.replace("🔥 ", "");
+          flow.name = flow.name.replace('🔥 ', '');
           return flow;
         });
       },

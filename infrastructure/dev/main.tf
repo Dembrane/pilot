faba3193aaed384c0ac5403d6f67fb89f32f2fc8
dtrn2048 <dat.tran@outlook.com>
2025-01-27 13:19:11 +0100

    Refactor network security rules in main.tf for clarity and organization (#144)

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -107,42 +107,48 @@ resource "azurerm_network_security_group" "private_nsg" {
   name                = "DBR-${var.environment}-Networks-private-NSG"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
 
-  security_rule {
-    name                       = "AllowFromPublicSubnet"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefixes    = azurerm_subnet.public_subnet[*].address_prefixes[0]
-    destination_address_prefix = "*"
-  }
+resource "azurerm_network_security_rule" "allow_from_public_subnet" {
+  name                        = "AllowFromPublicSubnet"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefixes     = azurerm_subnet.public_subnet[*].address_prefixes[0]
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.private_nsg.name
+}
 
-  security_rule {
-    name                       = "AllowAppGatewayHealthProbes"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_ranges    = ["8000", "8055", "5173"]
-    source_address_prefixes    = ["10.0.10.0/24"]
-    destination_address_prefix = "*"
-  }
+resource "azurerm_network_security_rule" "allow_app_gateway_health_probes" {
+  name                        = "AllowAppGatewayHealthProbes"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_ranges     = ["8055", "8000", "5173"]
+  source_address_prefixes     = ["10.0.10.0/24"]
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.private_nsg.name
+}
 
-  security_rule {
-    name                        = "AllowSSHFromBastion"
-    priority                    = 200
-    direction                   = "Inbound"
-    access                      = "Allow"
-    protocol                    = "Tcp"
-    source_port_range           = "*"
-    destination_port_range      = "22"
-    source_address_prefix       = azurerm_subnet.bastion_subnet.address_prefixes[0]
-    destination_address_prefix  = azurerm_subnet.ops_server_subnet.address_prefixes[0]
-  }
+resource "azurerm_network_security_rule" "allow_postgres_from_ops" {
+  name                        = "AllowPostgresFromOps"
+  priority                    = 210
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5432"
+  source_address_prefix       = "10.0.5.0/24"
+  destination_address_prefix  = "10.0.1.0/24"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.private_nsg.name
 }
 
 # NSG for Public Subnets

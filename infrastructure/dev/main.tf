3b7dc052ec22a8bf41691b316f6e4d491ec1b5ea
dat.tran@outlook.com <dat.tran@outlook.com>
2025-01-31 14:51:34 +0100

    Update PostgreSQL database password in Key Vault secret

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -1380,7 +1380,7 @@ resource "azurerm_cosmosdb_postgresql_cluster" "cosmo" {
 
 resource "azurerm_key_vault_secret" "database_url" {
   name         = "psql-database-url"
-  value        = "postgres://citus:placeholder_password@${azurerm_private_endpoint.psql_endpoint.private_service_connection[0].private_ip_address}:5432/citus?sslmode=require"
+  value        = "postgres://citus:1n1t14l_p@ssw0rd@${azurerm_private_endpoint.psql_endpoint.private_service_connection[0].private_ip_address}:5432/citus?sslmode=require"
   key_vault_id = azurerm_key_vault.DBR-dev-Backend-RuntimeConfig-KeyVault.id
 }
 

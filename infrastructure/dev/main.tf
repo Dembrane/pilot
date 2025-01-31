deabfce9c2b0dc10e0114efe4a4dcfdeb741d4b3
dat.tran@outlook.com <dat.tran@outlook.com>
2025-01-31 18:32:02 +0100

    Infra/add DB_SSL__REJECT_UNAUTHORIZED configuration for Directus

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -894,7 +894,7 @@ resource "azurerm_container_group" "directus" {
       DB_CLIENT = "${azurerm_key_vault_secret.directus_db_client.value}"
       DB_ACQUIRE_CONNECTION_TIMEOUT = 60000     
       DB_CONNECTION_STRING = "${azurerm_key_vault_secret.database_url.value}"
-      
+      DB_SSL__REJECT_UNAUTHORIZED="false"
       REDIS_ENABLED = "${azurerm_key_vault_secret.directus_redis_enabled.value}"
       REDIS = "${azurerm_key_vault_secret.directus_redis_url.value}"
 

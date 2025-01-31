bff6b6ccf785c720ec05ce91c87bec87446cb7c7
dtrn2048 <dat.tran@outlook.com>
2025-01-31 13:01:06 +0100

    Infra/fix directus db connection (#158)

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -63,6 +63,14 @@ resource "azurerm_subnet" "private_subnet" {
   }
 }
 
+# New subnet for private endpoints
+resource "azurerm_subnet" "private_endpoint_subnet" {
+  name                 = "DBR-${var.environment}-Networks-private-endpoint-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.7.0/24"]
+}
+
 resource "azurerm_subnet" "private_internal_subnet" {
   count                = 2
   name                 = "DBR-${var.environment}-Networks-private-subnet-new-${count.index + 1}"
@@ -1369,7 +1377,7 @@ resource "azurerm_private_endpoint" "psql_endpoint" {
   name                = "DBR-${var.environment}-PSQL-PrivateEndpoint"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.private_subnet[0].id
+  subnet_id           = azurerm_subnet.private_endpoint_subnet.id
 
   private_service_connection {
     name                           = "DBR-${var.environment}-PSQL-PrivateServiceConnection"

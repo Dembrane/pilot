a27f6766e6201130367607962b6fa089ad37c7d7
dtrn2048 <dat.tran@outlook.com>
2025-02-01 14:16:08 +0100

    Infra/update log level to trace for Directus container group (#168)

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -869,7 +869,7 @@ resource "azurerm_container_group" "directus" {
       CORS_ENABLED = "true"
       CORS_ORIGIN = "https://portal.dev.dembrane.com,https://dashboard.dev.dembrane.com,https://directus.dev.dembrane.com"
       CORS_CREDENTIALS = "true"
-      LOG_LEVEL = "debug"
+      LOG_LEVEL = "trace"
 
       SESSION_COOKIE_DOMAIN = "dev.dembrane.com"
       SESSION_COOKIE_SAME_SITE = "lax"
